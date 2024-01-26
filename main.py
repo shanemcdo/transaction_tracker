fae044777717c106d8dc1386b8ddc40aa9c4c7e1
Shane McDonough <shanemcdman@gmail.com>
2024-01-25 19:09:55 -0800

    Move autofit so it doesn't mess up the cal

--- main.py
+++ main.py
@@ -324,6 +324,7 @@ class Writer:
 			[{}, { 'format': self.formats['currency'] }],
 			headers = False
 		)
+		sheet.autofit()
 		start_col = max_col = max(max_col, col)
 		start_row = 0
 		start_row, _ = self.write_month_table(
@@ -350,7 +351,6 @@ class Writer:
 					i,
 					j
 				)
-		sheet.autofit()
 
 	def handle_month(self, month: int):
 		try:
